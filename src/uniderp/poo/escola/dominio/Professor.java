ffde7801555a42d85e3ccd48a0201de330cc30fb
Allan Augusto <137811222+AllanAugusto1@users.noreply.github.com>
2023-08-27 16:48:15 -0400

    Update Professor.java

---
+++ src/uniderp/poo/escola/dominio/Professor.java
@@ -0,0 +1,29 @@
+package uniderp.poo.escola.dominio;
+
+import java.time.LocalDate;
+
+public class Professor extends BasePessoa{
+    protected String registro;
+    protected LocalDate dataContratacao;
+
+    public String getRegistro(){
+        return registro;
+    }
+    public void setRegistro(String registro){
+        this.registro = registro;
+    }
+    public LocalDate getDataContratacao(){
+        return dataContratacao;
+    }
+    public void setDataContratacao(LocalDate dataContratacao){
+        this.dataContratacao = dataContratacao;
+    }
+    public Professor(int codigo, String nome, String endereco, String telefone, LocalDate dataNascimento, String rg,
+    String cpf, LocalDate dataInsercao, String registro, LocalDate dataContratacao) {
+        
+
+        super(codigo, nome, endereco, telefone, dataNascimento, rg, cpf, dataInsercao);
+}
+    public Professor() {
+    }  
+}
